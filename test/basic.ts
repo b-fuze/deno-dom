b6044a4095dc99d769eb1f81e39a10d56e8c88cb
b-fuze <b-fuze.root@live.com>
2020-06-10 01:33:22 -0400

    Print full tree

--- test/basic.ts
+++ test/basic.ts
@@ -1,5 +1,5 @@
 import { nodesFromString } from "../deno-dom-wasm.ts";
 import { readFileStrSync } from "https://deno.land/std/fs/read_file_str.ts";
 
-console.log(nodesFromString(readFileStrSync("./basic.html")));
+console.dir(nodesFromString(readFileStrSync("./basic.html")), { depth: null });
 
