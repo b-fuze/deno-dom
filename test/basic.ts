af89cd1434031002db62c2e4a43ec34aa7d09b93
b-fuze <b-fuze.root@live.com>
2020-06-10 01:35:35 -0400

    Infinity instead of null

--- test/basic.ts
+++ test/basic.ts
@@ -1,5 +1,5 @@
 import { nodesFromString } from "../deno-dom-wasm.ts";
 import { readFileStrSync } from "https://deno.land/std/fs/read_file_str.ts";
 
-console.dir(nodesFromString(readFileStrSync("./basic.html")), { depth: null });
+console.dir(nodesFromString(readFileStrSync("./basic.html")), { depth: Infinity });
 
