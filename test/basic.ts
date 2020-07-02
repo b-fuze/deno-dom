457c2be2fc4497d354b1e89b37257747eda0ab48
b-fuze <b-fuze.root@live.com>
2020-07-02 16:16:16 -0400

    Messy testing

--- test/basic.ts
+++ test/basic.ts
@@ -1,5 +1,18 @@
-import { nodesFromString } from "../deno-dom-wasm.ts";
+import { nodesFromString, NodeList } from "../deno-dom-wasm.ts";
 import { readFileStrSync } from "https://deno.land/std/fs/read_file_str.ts";
 
-console.dir(nodesFromString(readFileStrSync("./basic.html")), { depth: Infinity });
+const nodes = nodesFromString(readFileStrSync("./basic.html"));
+
+const childNodes = nodes.childNodes;
+console.log(childNodes instanceof NodeList);
+console.log(childNodes instanceof Array);
+console.log(Array.isArray(childNodes));
+console.log();
+console.log([] instanceof NodeList);
+console.log([] instanceof Array);
+console.log(Array.isArray([]));
+
+for (const child of nodes.childNodes) {
+  console.log("child", child + 1);
+}
 
---
+++ test/tree-wasm.ts
@@ -0,0 +1,22 @@
+import { nodesFromString } from "../deno-dom-wasm.ts";
+
+async function readAllString(reader: Deno.Reader, bufSize = 4096): Promise<string> {
+  const decoder = new TextDecoder();
+  const buffer = new Uint8Array(bufSize);
+  let accum = "";
+  let size: number | null = null;
+
+  while ((size = await reader.read(buffer)) !== null) {
+    accum += decoder.decode(buffer.subarray(0, <number> <unknown> size));
+  }
+
+  return accum;
+}
+
+const quiet = Deno.args[0] === "-q";
+const nodes = nodesFromString(await readAllString(Deno.stdin));
+
+if (!quiet) {
+  console.dir(nodes, { depth: Infinity });
+}
+
