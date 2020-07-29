20088d783b26b7b6a9ce8de6fbe4a8ed331d9b47
b-fuze <b-fuze.root@live.com>
2020-07-29 00:09:00 -0400

    Update Deno DOM Native to v0.1.1-alpha

--- deno-dom-native.ts
+++ deno-dom-native.ts
@@ -11,7 +11,7 @@ if (denoNativePluginPath) {
   // Open native plugin and register the native `parse` function
   Deno.openPlugin(denoNativePluginPath);
 } else {
-  const releaseUrl = "https://github.com/b-fuze/deno-dom/releases/download/v0.1.0-alpha";
+  const releaseUrl = "https://github.com/b-fuze/deno-dom/releases/download/v0.1.1-alpha";
   const pluginOptions: PerpareOptions = {
     name: "test_plugin",
 
