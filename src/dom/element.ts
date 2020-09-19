067219937e7cc7babd2243a46f90308dade356b0
josh <joshyrobot@gmail.com>
2020-09-18 19:23:47 -0600

    never include innerHTML in outerHTML for void elements, add some basic void element tests

--- src/dom/element.ts
+++ src/dom/element.ts
@@ -132,12 +132,7 @@ export class Element extends Node {
       case "source":
       case "track":
       case "wbr":
-        if (this.childNodes.length > 1) {
-          // What happened to the DOM lol
-          out += ">" + this.innerHTML + `</${ tagName }>`;
-        } else {
-          out += ">";
-        }
+        out += ">";
         break;
 
       default:
