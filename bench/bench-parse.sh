876525dc35e0cfea795bbc54e81405f25a4f5f2f
b-fuze <b-fuze.root@live.com>
2021-01-06 11:00:45 -0500

    refactor: reorganize bench for parsing

--- bench/bench-parse.sh
+++ bench/bench-parse.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo -n "Deno DOM WASM - "
-deno run --allow-read ./bench-wasm.ts | grep time:
+deno run --allow-read ./bench-wasm-parse.ts | grep time:
 
 echo -n "Deno DOM Native - "
-deno run -A --unstable ./bench-native.ts | grep time:
+deno run -A --unstable ./bench-native-parse.ts | grep time:
 
 echo -n "Node.js Parse5 - "
 node ./bench-parse5.js
