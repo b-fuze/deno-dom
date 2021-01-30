f622f15fd8510684de0c12ac40405f31fd645ea2
Liam Murphy <liampm32@gmail.com>
2021-01-30 23:22:35 +1100

    fix: stop arrays being counted as instances of subclasses of arrays

---
+++ test/instanceof.ts
@@ -0,0 +1,34 @@
+import { DOMParser, NodeList, nodesFromString } from "../deno-dom-wasm.ts";
+import { assert } from "https://deno.land/std@0.85.0/testing/asserts.ts";
+
+Deno.test("Array instanceof Array", () => {
+  assert([] instanceof Array);
+  assert(Array.isArray([]));
+});
+
+Deno.test("NodeList not instanceof Array", () => {
+  const nodes = nodesFromString(
+    Deno.readTextFileSync(new URL("./basic.html", import.meta.url)),
+  );
+  assert(!(nodes instanceof Array));
+  assert(!Array.isArray(nodes));
+});
+
+Deno.test("HTMLCollection not instanceof Array", () => {
+  const nodes = nodesFromString(
+    Deno.readTextFileSync(new URL("./basic.html", import.meta.url)),
+  );
+  assert(!(nodes.children instanceof Array));
+  assert(!Array.isArray(nodes.children));
+});
+
+Deno.test("Subclass instanceof Array", () => {
+  class Foo extends Array {}
+  assert(new Foo() instanceof Array);
+  assert(Array.isArray(new Foo()));
+});
+
+Deno.test("Array not instanceof subclass", () => {
+  class Foo extends Array {}
+  assert(!([] instanceof Foo));
+});
