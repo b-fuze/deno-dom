748be040fd14a33186935966ea3ea008ce4fadfb
Nathan Brooks <natebrooks004@gmail.com>
2021-09-25 20:54:22 -0700

    node.ts: implement Node.childElementCount

---
+++ test/units/Node-childElementCount.ts
@@ -0,0 +1,33 @@
+import { Document } from "../../deno-dom-wasm.ts";
+import { assertEquals } from "https://deno.land/std@0.85.0/testing/asserts.ts";
+
+Deno.test("Document.childElementCount", () => {
+  const document = new Document();
+  assertEquals(document.childElementCount, 0);
+});
+
+Deno.test("Element.childElementCount", () => {
+  const document = new Document();
+  const list = document.createElement("ul");
+  for (let i = 0; i < 3; i++) {
+    list.appendChild(document.createElement("li"));
+  }
+  assertEquals(list.childElementCount, 3);
+});
+
+Deno.test("Element.childElementCount", () => {
+  const document = new Document();
+  const list = document.createElement("ul");
+  for (let i = 0; i < 3; i++) {
+    list.appendChild(document.createElement("li"));
+  }
+  assertEquals(list.childElementCount, 3);
+});
+
+Deno.test("Element.childElementCount with non-node", () => {
+  const document = new Document();
+  const divider = document.createElement("div");
+  divider.appendChild(document.createElement("p"));
+  divider.appendChild(document);
+  assertEquals(divider.childElementCount, 1);
+});
