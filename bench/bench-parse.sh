0097c3c32617e08e4952d80e9c9f8fba45920d53
b-fuze <b-fuze.root@live.com>
2021-11-14 15:24:08 -0500

    fix(bench): filter stderr for parse bench

--- bench/bench-parse.sh
+++ bench/bench-parse.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo -n "Deno DOM WASM   - "
-deno run --allow-read ./bench-wasm-parse.ts | grep time:
+deno run --allow-read ./bench-wasm-parse.ts 2>&1 | grep time:
 
 echo -n "Deno DOM Native - "
-deno run -A --unstable ./bench-native-parse.ts | grep time:
+deno run -A --unstable ./bench-native-parse.ts 2>&1 | grep time:
 
 echo -n "Node.js Parse5  - "
 node ./bench-parse5.js
