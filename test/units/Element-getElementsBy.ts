e9a4750f27c24ef8e025a8ba0d7821b681bfd2a9
b-fuze <b-fuze.root@live.com>
2022-02-27 08:37:00 -0500

    chore: Element.getElementsBy* tests

---
+++ test/units/Element-getElementsBy.ts
@@ -0,0 +1,128 @@
+import { DOMParser, Element } from "../../deno-dom-wasm.ts";
+import { assertStrictEquals as assertEquals, assertExists } from "https://deno.land/std@0.85.0/testing/asserts.ts";
+
+Deno.test("Element.getElementsByTagName", () => {
+  const doc = new DOMParser().parseFromString(
+    `
+      <div class=main>
+        <aside>
+          <img>
+          <article class=an-article>
+            <p class=this-p>Foo bar baz</p>
+          </article>
+        </aside>
+      </div>
+    `,
+    "text/html",
+  )!;
+  const divMain = doc.querySelector("div")!;
+  const aside = divMain.getElementsByTagName("aside")[0];
+  const article = divMain.getElementsByTagName("article")[0];
+
+  assertExists(aside);
+  assertExists(article);
+
+  const img = article.previousElementSibling!;
+  const mainP = divMain.getElementsByTagName("p")[0];
+  const articleP = article.getElementsByTagName("P")[0];
+  const articlePClass = article.getElementsByClassName("this-p")[0];
+
+  assertExists(img);
+  assertExists(mainP);
+  assertExists(articleP);
+
+  const imgParentArticle = (img!.parentElement! as Element).getElementsByTagName("article")[0];
+
+  assertEquals(aside.tagName, "ASIDE");
+  assertEquals(article.tagName, "ARTICLE");
+  assertEquals(img.tagName, "IMG");
+  assertEquals(mainP.tagName, "P");
+  assertEquals(articleP.tagName, "P");
+  assertEquals(articleP, articlePClass);
+  assertEquals(mainP.parentNode, article);
+  assertEquals(imgParentArticle, article);
+});
+
+Deno.test("Element.getElementsByClassName", () => {
+  const doc = new DOMParser().parseFromString(
+    `
+      <div class=main>
+        <aside class=an-aside>
+          <img>
+          <article class=an-article>
+            <p class=this-p>Foo bar baz</p>
+          </article>
+        </aside>
+      </div>
+    `,
+    "text/html",
+  )!;
+  const divMain = doc.querySelector(".main")!;
+  const aside = divMain.getElementsByClassName("an-aside")[0];
+  const article = divMain.getElementsByClassName("an-article")[0];
+
+  assertExists(aside);
+  assertExists(article);
+
+  const img = article.previousElementSibling!;
+  const mainP = divMain.getElementsByClassName("this-p")[0];
+  const articleP = article.getElementsByClassName("this-p")[0];
+
+  assertExists(img);
+  assertExists(mainP);
+  assertExists(articleP);
+
+  const imgParentArticle = (img!.parentElement! as Element).getElementsByClassName("an-article")[0];
+
+  assertEquals(aside.tagName, "ASIDE");
+  assertEquals(article.tagName, "ARTICLE");
+  assertEquals(img.tagName, "IMG");
+  assertEquals(mainP.tagName, "P");
+  assertEquals(articleP.tagName, "P");
+  assertEquals(mainP, articleP);
+  assertEquals(mainP.parentNode, article);
+  assertEquals(imgParentArticle, article);
+});
+
+Deno.test("Element.getElementById", () => {
+  const doc = new DOMParser().parseFromString(
+    `
+      <div id=main>
+        <aside id=an-aside>
+          <img>
+          <article id=an-article>
+            <p id=this-p>Foo bar baz</p>
+          </article>
+        </aside>
+      </div>
+    `,
+    "text/html",
+  )!;
+  const divMain = doc.querySelector("#main")!;
+  const aside = divMain.getElementById("an-aside")!;
+  const article = divMain.getElementById("an-article")!;
+
+  assertExists(aside);
+  assertExists(article);
+
+  const img = article.previousElementSibling!;
+  const mainP = divMain.getElementById("this-p")!;
+  const articleP = article.getElementById("this-p")!;
+  const articlePClass = article.getElementById("this-p")!;
+
+  assertExists(img);
+  assertExists(mainP);
+  assertExists(articleP);
+
+  const imgParentArticle = (img!.parentElement! as Element).getElementById("an-article");
+
+  assertEquals(aside.tagName, "ASIDE");
+  assertEquals(article.tagName, "ARTICLE");
+  assertEquals(img.tagName, "IMG");
+  assertEquals(mainP.tagName, "P");
+  assertEquals(articleP.tagName, "P");
+  assertEquals(articleP, articlePClass);
+  assertEquals(mainP.parentNode, article);
+  assertEquals(imgParentArticle, article);
+});
+
