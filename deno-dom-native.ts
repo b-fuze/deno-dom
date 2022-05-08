7652e15a418d99f5ae2ba6728dcaa779c7e64625
b-fuze <b-fuze.root@live.com>
2022-05-08 00:50:28 -0400

    v0.1.24-alpha

--- deno-dom-native.ts
+++ deno-dom-native.ts
@@ -35,7 +35,8 @@ if (denoNativePluginPath) {
   // Download the plugin
   dylib = await Plug.prepare({
     name: "plugin",
-    url: "https://github.com/b-fuze/deno-dom/releases/download/v0.1.23-alpha-artifacts/",
+    url:
+      "https://github.com/b-fuze/deno-dom/releases/download/v0.1.23-alpha-artifacts/",
   }, symbols);
 }
 
