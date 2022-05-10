cdb99e55d0cbe76568a3f3eb6c53ebe0fa4819bf
Okku <40242277+0kku@users.noreply.github.com>
2022-05-11 01:51:32 +0300

    test: Element.classList.value

--- test/units/Element-classList.ts
+++ test/units/Element-classList.ts
@@ -1,6 +1,23 @@
 import { DOMParser } from "../../deno-dom-wasm.ts";
 import { assert } from "https://deno.land/std@0.85.0/testing/asserts.ts";
 
+Deno.test("Element.classList.value", () => {
+  const doc = new DOMParser().parseFromString("<div class='foo bar'></div>", "text/html")!;
+  const div = doc.querySelector("div")!;
+  assert(div.classList.value === "foo bar");
+  assert(div.classList.contains("foo"));
+  assert(div.classList.contains("bar"));
+  div.classList.value = "fiz  baz fiz";
+  assert(div.classList.value === "fiz  baz fiz");
+  assert(!div.classList.contains("foo"));
+  assert(!div.classList.contains("bar"));
+  assert(div.classList.contains("fiz"));
+  assert(div.classList.contains("baz"));
+  div.classList.add("qux");
+  // @ts-ignore
+  assert(div.classList.value === "fiz baz qux");
+});
+
 Deno.test("Element.classList.add", () => {
   const doc = new DOMParser().parseFromString("<div></div>", "text/html")!;
   const div = doc.querySelector("div")!;
