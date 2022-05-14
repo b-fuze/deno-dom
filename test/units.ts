3e6349b22800e86db700807798cf10c1ca6d098b
Okku <40242277+0kku@users.noreply.github.com>
2022-05-14 18:06:43 +0300

    fix: test runner on Windows

--- test/units.ts
+++ test/units.ts
@@ -1,14 +1,6 @@
-import { dirname, join } from "https://deno.land/std@0.85.0/path/mod.ts";
-
-const unitDir = join(dirname(new URL(import.meta.url).pathname), "units");
-const units = Array.from(Deno.readDirSync(unitDir))
-  .filter((file) => file.isFile && file.name.endsWith(".ts"))
-  .map((file) => join("units", file.name));
-
-// Change into the unitDir
-Deno.chdir(unitDir);
-
-// Import all unit files
-for (const file of units) {
-  await import("./" + file);
-}
+const unitDir = new URL("units", import.meta.url);
+await Promise.all(
+  Array.from(Deno.readDirSync(unitDir))
+    .filter((file) => file.isFile && file.name.endsWith(".ts"))
+    .map((file) => import(`./units/${file.name}`)),
+);
