dbf18fb96bf84726303c52039998678052fe5830
b-fuze <b-fuze.root@live.com>
2025-07-06 18:41:13 -0400

    feat: dynamically import native WASM module only on supported platforms
    Closes #192

--- build/deno-wasm/deno-wasm.js
+++ build/deno-wasm/deno-wasm.js
@@ -1,15 +1,34 @@
 // FIXME: find a better way to use the raw output from wasm-pack
 // instead of replacing the JS entrypoint wholesale like this
 
-import {
+function hasSuitableDenoVersion(version) {
+    const [major, minor] = version.split(".").map(Number);
+    return major >= 2 && minor >= 1;
+}
+
+const {
     memory,
     __wbindgen_export_0,
     __wbindgen_malloc,
     __wbindgen_realloc,
     __wbindgen_free,
-    parse as parse_wasm,
-    parse_frag as parse_frag_wasm,
-} from "./deno-wasm_bg.wasm";
+    parse_wasm,
+    parse_frag_wasm,
+} = await (async () => {
+    let moduleImports;
+
+    if (typeof Deno === "object" && hasSuitableDenoVersion(Deno.version?.deno || "0.0.0")) {
+        moduleImports = await import("./deno-wasm_bg.wasm");
+    } else {
+        moduleImports = (await import("./deno-wasm_bg-wasm.js")).default;
+    }
+
+    const {parse, parse_frag, ...remappedImports} = moduleImports;
+    remappedImports.parse_wasm = parse;
+    remappedImports.parse_frag_wasm = parse_frag;
+
+    return remappedImports;
+})();
 
 {
     const table = __wbindgen_export_0;
---
+++ html-parser/wasm/deno-wasm_bg-wasm.js
@@ -0,0 +1,9 @@
+const imports = {
+  "./env.js": { now: performance.now },
+  "./wbg.js": { __wbindgen_init_externref_table() {} },
+};
+
+const moduleBytes = Uint8Array.from(atob("WASM_BASE64"), (c) => c.charCodeAt(0));
+const { instance } = await WebAssembly.instantiate(moduleBytes, imports);
+
+export default instance.exports;
